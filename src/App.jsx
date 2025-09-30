ba472f8134c2f5a8c78ce36e26fb3da8fe3fb8e4
Parth Tiwari <youremail@gmail.com>
2025-09-30 21:46:20 +0530

    Rename stairs.jsx to Stairs.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Agence from './pages/agence'
-import Projects from './pages/Projects'
+import Projects from './pages/project'
 import Navbar from './components/Navigation/Navbar'
 import FullScreenNav from './components/Navigation/FullScreenNav'
 
--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from '@gsap/react'
 import { useRef } from 'react'
-import ProjectCard from '../components/project/ProjectCard'  // projects se project
+import ProjectCard from '../components/Project/ProjectCard'  // projects se project
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
